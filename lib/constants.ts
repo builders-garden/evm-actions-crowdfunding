623882cc66bfcba18e3fe5e444c80cd78100778e
lim0n3 <simonestaffa96@gmail.com>
2024-07-14 02:13:47 +0200

    change contract addresses

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const NATIVE_TOKEN: `0x${string}` =
   "0x0000000000000000000000000000000000000000";
 export const STANDARD_TOKEN_ADDRESS: `0x${string}` =
-    "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03"; // NOUNS token address on Base
+    "0x0a93a7BE7e7e426fC046e204C44d6b03A302b631"; // NOUNS token address on Base
 export const CROWDFUNDING_CONTRACT_ADDRESS: `0x${string}` =
-    "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03"; // Crowdfunding contract address on Base //TODO: Update this
+    "0x37bf6CA5aF1Ad5C29D7B74438FA6D653872385C5"; // Crowdfunding contract address on Base //TODO: Update this
